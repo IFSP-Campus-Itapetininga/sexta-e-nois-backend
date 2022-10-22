266cda8a2255f2b30ae448ce302967311b231b83
Thomas Nishiduka <thomas.nishiduka@gmail.com>
2022-10-22 19:45:35 -0300

    feat: adicionado morgan para log de requests

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ require('dotenv/config')
 
 const express = require('express')
 const cors = require('cors')
+const morgan = require('morgan')
 const routes = require('./routes')
 
 const app = express()
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use(cors())
 app.use(express.json())
 
