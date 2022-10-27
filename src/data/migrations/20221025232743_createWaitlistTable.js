6c9a40cfa5db082bad1c0aa61760bdc75b0c9dfc
Walace Soares <walacesoares28@gmail.com>
2022-10-27 19:24:31 -0300

    feat: criação migration tabela waitlist

---
+++ src/data/migrations/20221025232743_createWaitlistTable.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('waitlist', (table) => {
+    table.increments('id').unsigned().primary()
+    table.string('nome').notNullable()
+    table.tinyint('alfabetizado').nullable()
+    table.string('escolaridade').nullable()
+    table.dateTime('dataNascimento').nullable()
+    table.dateTime('dataCadastro').nullable()
+    table.string('nomeResponsavel').nullable()
+    table.integer('telefone').nullable()
+  })
+}
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('waitlist')
+}
