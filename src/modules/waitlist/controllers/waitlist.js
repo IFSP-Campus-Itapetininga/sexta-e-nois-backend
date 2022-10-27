3ea85042ecaa33302fffa534377e661dc59c900d
Walace Soares <walacesoares28@gmail.com>
2022-10-27 19:23:39 -0300

    feat: criação da routes, controller e model do modulo waitlist

---
+++ src/modules/waitlist/controllers/waitlist.js
@@ -0,0 +1,64 @@
+const Waitlist = require('../models/Waitlist')
+
+const waitlistModel = Waitlist()
+
+const create = async (req, res) => {
+  const { nome, alfabetizado, escolaridade, dataNascimento, dataCadastro, nomeResponsavel, telefone } = req.body
+
+  try {
+    await waitlistModel.create({ nome, alfabetizado, escolaridade, dataNascimento, dataCadastro, nomeResponsavel, telefone })
+
+    res.status(201).send()
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+}
+
+const list = async (req, res) => {
+  try {
+    const waitlists = await waitlistModel.list()
+
+    res.send(waitlists)
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+}
+
+const find = async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const waitlists = await waitlistModel.find(id)
+
+    res.send(waitlists)
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+}
+
+const update = async (req, res) => {
+  const { id } = req.params
+  const { nome, alfabetizado, escolaridade, dataNascimento, dataCadastro, nomeResponsavel, telefone } = req.body
+
+  try {
+    await waitlistModel.update(id, { nome, alfabetizado, escolaridade, dataNascimento, dataCadastro, nomeResponsavel, telefone })
+
+    res.status(204).send()
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+}
+
+const remove = async (req, res) => {
+  const { id } = req.params
+
+  try {
+    await waitlistModel.remove(id)
+
+    res.status(204).send()
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+}
+
+module.exports = { create, list, find, update, remove }
