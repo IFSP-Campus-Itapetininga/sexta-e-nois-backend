f11a1dffa45e1b34c080ea8bcc31641d4af89d6c
Thomas Nishiduka <thomas.nishiduka@gmail.com>
2022-10-29 13:59:44 -0300

    fix: corrigido erros de console

--- src/modules/lunchs/models/Product.js
+++ src/modules/lunchs/models/Product.js
@@ -15,14 +15,14 @@ module.exports = () => {
    * @param {number} page
    * @param {number} limit
    */
-  const list = async (page = 0, limit = 10, search) => {
+  const list = async (page = 0, limit = 10, search = '') => {
     const [count, data] = await Promise.all([
       knex.from(TABLE_NAME).count(),
       knex
         .select('*')
         .from(TABLE_NAME)
         .modify(function (queryBuilder) {
-          if (!!search) {
+          if (search) {
             queryBuilder.where('titulo', 'LIKE', `%${search}%`)
           }
         })
@@ -54,28 +54,6 @@ module.exports = () => {
       .then((row) => row)
     if (!result) {
       throw new Error('Product not found')
-    }
-
-    return result
-  }
-
-  /**
-   *
-   * @param {number} id
-   * @returns {*}
-   */
-  const findMany = async (id) => {
-    const result = await knex
-      .select('*')
-      .from(TABLE_NAME)
-      .whereIn('id', id)
-      .then((row) => row)
-
-    if (id.length !== result.length) {
-      const resultIds = result.map((item) => item.id)
-      const notIncluded = id.filter((item) => !resultIds.includes(item))
-
-      throw new Error(`Product ${notIncluded[0]} not find`)
     }
 
     return result
