b0a22a0eb773230fc30cacb32595b96a9e91296f
gabrielBFerreira <gabriel.brisolla.ferreira@gmail.com>
2022-11-08 02:29:20 -0300

    feat: seeds de usuarios e roles criadas

---
+++ src/data/seeds/users.js
@@ -0,0 +1,16 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex('usuario').del()
+  await knex('usuario').insert([
+    {
+      id: 1,
+      nome: 'admin001',
+      senha: '$2a$12$FM1lVPdMgQzYmPu8T8oqIebElMhz2TwiCYzO5z24RM20bMccT6gTu', // password = 123; NÃO USAR este usuário em produção; apenas para testes; apagar este arquivo ao final do projeto.
+      idPapel: 1
+    }
+  ])
+}
