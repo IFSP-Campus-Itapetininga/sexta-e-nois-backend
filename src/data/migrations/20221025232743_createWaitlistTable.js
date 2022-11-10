394cc2dce70d13b00dc9ce9a84063ee20777354d
Walace Soares <walacesoares28@gmail.com>
2022-11-09 21:01:02 -0300

    refactor: alterado tipo do campo telefone e campo dataCadastro para registro automatico

--- src/data/migrations/20221025232743_createWaitlistTable.js
+++ src/data/migrations/20221025232743_createWaitlistTable.js
@@ -3,16 +3,16 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-  return knex.schema.createTable('waitlist', (table) => {
+  return knex.schema.createTable('lista_espera', (table) => {
     table.increments('id').unsigned().primary()
     table.string('nome').notNullable()
     table.tinyint('alfabetizado').nullable()
     table.string('escolaridade').nullable()
     table.string('oficina').nullable()
     table.dateTime('dataNascimento').nullable()
-    table.dateTime('dataCadastro').nullable()
+    table.dateTime('dataCadastro').defaultTo(knex.fn.now())
     table.string('nomeResponsavel').nullable()
-    table.integer('telefone').nullable()
+    table.string('telefone').nullable()
   })
 }
 /**
@@ -20,5 +20,5 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-  return knex.schema.dropTable('waitlist')
+  return knex.schema.dropTable('lista_espera')
 }
