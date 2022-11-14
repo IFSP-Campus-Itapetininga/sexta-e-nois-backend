f37d70693f6a91c03a008fbb54bd85027d474492
Gabriel Brisolla Ferreira <gabriel.brisolla.ferreira@gmail.com>
2022-11-14 04:14:47 -0300

    refactor: alterando username no seed
    Co-authored-by: Matheus Pelegrinetti <37592578+Pelegrinetti@users.noreply.github.com>

--- src/data/seeds/users.js
+++ src/data/seeds/users.js
@@ -8,7 +8,7 @@ exports.seed = async function (knex) {
   await knex('usuario').insert([
     {
       id: 1,
-      nome: 'admin001',
+      nome: 'Administrador',
       senha: '$2a$12$FM1lVPdMgQzYmPu8T8oqIebElMhz2TwiCYzO5z24RM20bMccT6gTu', // password = 123; NÃO USAR este usuário em produção; apenas para testes; apagar este arquivo ao final do projeto.
       idPapel: 1
     }
