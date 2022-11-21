824547443a546f2c5ca8c2ed2e19a049e1171f82
Walace Soares <walacesoares28@gmail.com>
2022-11-21 19:37:01 -0300

    feat: criacao controller student e responsavel

---
+++ src/modules/secretary/controllers/responsible.js
@@ -0,0 +1,76 @@
+const Responsible = require('../models/Responsible')
+const { ValidationResponsible } = require('../utils')
+
+const responsibleModel = Responsible()
+
+const create = async (req, res) => {
+  const data = req.body
+  const validation = ValidationResponsible.validate(data)
+
+  if (validation.error) {
+    res.status(400).json({ error: validation.error.details })
+    return
+  }
+  try {
+    await responsibleModel.create(data)
+
+    res.status(201).send()
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+}
+
+const list = async (req, res) => {
+  try {
+    const students = await responsibleModel.list()
+
+    res.send(students)
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+}
+
+const find = async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const student = await responsibleModel.find(id)
+
+    res.send(student)
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+}
+
+const update = async (req, res) => {
+  const { id } = req.params
+  const data = req.body
+  const validation = ValidationResponsible.validate(data)
+
+  if (validation.error) {
+    res.status(400).json({ error: validation.error.details })
+    return
+  }
+
+  try {
+    await responsibleModel.update(id, data)
+
+    res.status(204).send()
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+}
+
+const remove = async (req, res) => {
+  const { id } = req.params
+
+  try {
+    await responsibleModel.remove(id)
+
+    res.status(204).send()
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+}
+
+module.exports = { create, list, find, update, remove }
