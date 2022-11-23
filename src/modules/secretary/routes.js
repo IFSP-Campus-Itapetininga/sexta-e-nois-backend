1a3412c9291cedec0a624ddfc4686c915261fde8
Walace Soares <walacesoares28@gmail.com>
2022-11-23 19:46:14 -0300

    feat: rotas telefone e endereco

--- src/modules/secretary/routes.js
+++ src/modules/secretary/routes.js
@@ -1,6 +1,11 @@
 const { Router } = require('express')
 const studentController = require('./controllers/student')
 const responsibleController = require('./controllers/responsible')
+const PhoneController = require('./controllers/phone')
+const AddressController = require('./controllers/address')
+const RegistrationController = require('./controllers/registration')
+const AttendanceController = require('./controllers/attendance')
+const ClassController = require('./controllers/class')
 
 const ROUTES_PREFIX = '/secretary'
 const router = Router()
@@ -24,6 +29,66 @@ router.get('/responsible/:id', responsibleController.find)
 router.put('/responsible/:id', responsibleController.update)
 
 router.delete('/responsible/:id', responsibleController.remove)
+
+router.post('/phone', PhoneController.create)
+
+router.get('/phone', PhoneController.list)
+
+router.get('/phone/:id', PhoneController.find)
+
+router.put('/phone/:id', PhoneController.update)
+
+router.delete('/phone/:id', PhoneController.remove)
+
+router.post('/address', AddressController.create)
+
+router.get('/address', AddressController.list)
+
+router.get('/address/:id', AddressController.find)
+
+router.put('/address/:id', AddressController.update)
+
+router.delete('/address/:id', AddressController.remove)
+
+router.post('/registration', RegistrationController.create)
+
+router.get('/registration', RegistrationController.list)
+
+router.get('/registration/:id', RegistrationController.find)
+
+router.put('/registration/:id', RegistrationController.update)
+
+router.delete('/registration/:id', RegistrationController.remove)
+
+router.post('/attendance', AttendanceController.create)
+
+router.get('/attendance', AttendanceController.list)
+
+router.get('/attendance/:id', AttendanceController.find)
+
+router.put('/attendance/:id', AttendanceController.update)
+
+router.delete('/attendance/:id', AttendanceController.remove)
+
+router.post('/attendance', AttendanceController.create)
+
+router.get('/attendance', AttendanceController.list)
+
+router.get('/attendance/:id', AttendanceController.find)
+
+router.put('/attendance/:id', AttendanceController.update)
+
+router.delete('/attendance/:id', AttendanceController.remove)
+
+router.post('/class', ClassController.create)
+
+router.get('/class', ClassController.list)
+
+router.get('/class/:id', ClassController.find)
+
+router.put('/class/:id', ClassController.update)
+
+router.delete('/class/:id', ClassController.remove)
 
 module.exports = {
   ROUTES_PREFIX,
