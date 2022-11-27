1e865af161c32b8a3e98798c2b168da598414946
Gabriel Brisolla Ferreira <gabriel.brisolla.ferreira@gmail.com>
2022-11-27 20:45:50 -0300

    refactor: atualizando nome de tabela

--- src/modules/vendor/models/Contact.js
+++ src/modules/vendor/models/Contact.js
@@ -1,7 +1,7 @@
 const knex = require('../../../data/connection')
 
 module.exports = () => {
-  const TABLE_NAME = 'contato'
+  const TABLE_NAME = 'contatoFornecedor'
 
   const create = async data => await knex.insert(data).into(TABLE_NAME)
 
