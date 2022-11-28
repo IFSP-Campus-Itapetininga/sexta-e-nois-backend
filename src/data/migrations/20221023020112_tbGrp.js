d92fe2ab31ef8b6ef104d022d910f1223040615b
sav5rv <wsavioli@gmail.com>
2022-11-27 21:53:49 -0300

    exclusão tabelas

--- src/data/migrations/20221023020112_tbGrp.js
+++ src/data/migrations/20221023020112_tbGrp.js
@@ -5,8 +5,8 @@
 exports.up = function(knex) {
     return knex.schema.createTable('tbGrp', (table) => {
         table.increments('id').unsigned().primary()
-        table.integer('idProf').unsigned()
-        table.foreign('idProf').references('tbProf.id')
+        //table.integer('idProf').unsigned()
+        //table.foreign('idProf').references('tbProf.id')
         table.string('descricao').nullable()
         table.timestamp('criadoEm').defaultTo(knex.fn.now())
         table.timestamp('alteradoEm').defaultTo(knex.fn.now())
