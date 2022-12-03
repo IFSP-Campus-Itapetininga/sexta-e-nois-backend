e39d9faa68158e6473c80950fb9e08dfab870384
Walace Soares <walacesoares28@gmail.com>
2022-12-03 14:26:18 -0300

    feat: rotas atualizadas

--- src/modules/secretary/routes.js
+++ src/modules/secretary/routes.js
@@ -6,6 +6,9 @@ const AddressController = require('./controllers/address')
 const RegistrationController = require('./controllers/registration')
 const AttendanceController = require('./controllers/attendance')
 const ClassController = require('./controllers/class')
+const WorkshopController = require('./controllers/workshop')
+const InstructorController = require('./controllers/instructor')
+const GroupController = require('./controllers/group')
 
 const ROUTES_PREFIX = '/secretary'
 const router = Router()
@@ -89,6 +92,36 @@ router.get('/class/:id', ClassController.find)
 router.put('/class/:id', ClassController.update)
 
 router.delete('/class/:id', ClassController.remove)
+
+router.post('/workshop', WorkshopController.create)
+
+router.get('/workshop', WorkshopController.list)
+
+router.get('/workshop/:id', WorkshopController.find)
+
+router.put('/workshop/:id', WorkshopController.update)
+
+router.delete('/workshop/:id', WorkshopController.remove)
+
+router.post('/instructor', InstructorController.create)
+
+router.get('/instructor', InstructorController.list)
+
+router.get('/instructor/:id', InstructorController.find)
+
+router.put('/instructor/:id', InstructorController.update)
+
+router.delete('/instructor/:id', InstructorController.remove)
+
+router.post('/group', GroupController.create)
+
+router.get('/group', GroupController.list)
+
+router.get('/group/:id', GroupController.find)
+
+router.put('/group/:id', GroupController.update)
+
+router.delete('/group/:id', GroupController.remove)
 
 module.exports = {
   ROUTES_PREFIX,
