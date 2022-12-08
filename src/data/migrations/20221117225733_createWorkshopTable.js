800526473d7dcdbdd8bb817480c66d2a9c929a18
Walace Soares <walacesoares28@gmail.com>
2022-12-07 21:12:59 -0300

    fix: tabela oficina

--- src/data/migrations/20221117225733_createWorkshopTable.js
+++ src/data/migrations/20221117225733_createWorkshopTable.js
@@ -5,7 +5,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable('oficina', (table) => {
     table.increments('id').unsigned().primary()
-    table.string('nome').defaultTo(knex.fn.now())
+    table.string('nome').notNullable()
     table.integer('idade_minima').notNullable()
     table.integer('idade_maxima').notNullable()
     table.string('outras_restricoes').nullable()
