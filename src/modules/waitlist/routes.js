dd97e2bb9b7bd327037a1b2c5fa5f39c4dd91ddb
Walace Soares <walacesoares28@gmail.com>
2022-12-07 21:19:20 -0300

    feat: autenticação

--- src/modules/waitlist/routes.js
+++ src/modules/waitlist/routes.js
@@ -1,18 +1,20 @@
 const { Router } = require('express')
 const waitlistController = require('./controllers/waitlist')
 
+const authentication = require('../../utils/middlewares/authentication')
+
 const ROUTES_PREFIX = '/waitlist'
 const router = Router()
 
-router.post('/', waitlistController.create)
+router.post('/', authentication.verifyRole(['Administrador', 'Secretário']), waitlistController.create)
 
-router.get('/', waitlistController.list)
+router.get('/', authentication.verifyRole(['Administrador', 'Secretário']), waitlistController.list)
 
-router.get('/:id', waitlistController.find)
+router.get('/:id', authentication.verifyRole(['Administrador', 'Secretário']), waitlistController.find)
 
-router.put('/:id', waitlistController.update)
+router.put('/:id', authentication.verifyRole(['Administrador', 'Secretário']), waitlistController.update)
 
-router.delete('/:id', waitlistController.remove)
+router.delete('/:id', authentication.verifyRole(['Administrador', 'Secretário']), waitlistController.remove)
 
 module.exports = {
   ROUTES_PREFIX,
