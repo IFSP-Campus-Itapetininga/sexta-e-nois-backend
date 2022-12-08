8a272b7bc6c1e938300dbd1fd5abffd68147a8c0
Thomas Nishiduka <thomas.nishiduka@gmail.com>
2022-12-08 11:57:59 -0300

    feat: removido ssl

--- src/app.js
+++ src/app.js
@@ -4,9 +4,6 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const routes = require('./routes')
-const https = require('https')
-const fs = require('fs')
-
 const app = express()
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
@@ -18,15 +15,6 @@ app.use('/v1', routes)
 app.use((err, req, res, next) => {
   res.status(500).send({ error: err.message })
 })
-
-if (process.env.HTTPS) {
-  const cert = fs.readFileSync(process.env.CERT_PATH)
-  const key = fs.readFileSync(process.env.CERT_SECRET_PATH)
-
-  const httpsServer = https.createServer({ key, cert }, app)
-
-  httpsServer.listen(443)
-}
 
 module.exports = {
   run: () => {
